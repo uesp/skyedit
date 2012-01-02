b11da2ca7d8a848f45990212927f4390f72722a6
Dave Humphrey <dave@uesp.net>
2012-01-02 10:43:13 -0500

    misc white space edit

--- dialogs/SrMiscView.cpp
+++ dialogs/SrMiscView.cpp
@@ -20,14 +20,7 @@
  * Begin Local Definitions
  *
  *=========================================================================*/
-//#ifdef _DEBUG
-//  #define new DEBUG_NEW
-//  #undef THIS_FILE
-//  static char THIS_FILE[] = __FILE__;
-//#endif
-
-  IMPLEMENT_DYNCREATE(CSrMiscView, CSrRecordDialog);
-
+	IMPLEMENT_DYNCREATE(CSrMiscView, CSrRecordDialog);
 /*===========================================================================
  *		End of Local Definitions
  *=========================================================================*/
